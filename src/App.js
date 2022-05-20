0a17a77933f34062a413ac59f855efce6bdd4f33
Pankaj Singh <pankaj.singh@enyotalearning.com>
2022-05-20 13:09:02 +0530

    MERGE

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import { makeStyles } from "@material-ui/core";
+import Homepage from "./Pages/HomePage";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import CoinPage from "./Pages/CoinPage";
+import Header from "./components/Header";
+
+const useStyles = makeStyles(() => ({
+  App: {
+    backgroundColor: "#14161a",
+    color: "white",
+    minHeight: "100vh",
+  },
+}));
+
+function App() {
+  const classes = useStyles();
+
+  return (
+    <>
+    <Router>
+      <div className={classes.App}>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Homepage />} exact />
+          <Route path="/coins/:id" element={<CoinPage />} exact />
+        </Routes>
+      </div>
+    </Router>
+    </>
+  );
+}
+
+export default App;
